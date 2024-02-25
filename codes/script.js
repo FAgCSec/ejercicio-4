771a721b69530092a27b7d7249a8cee28d3520de
FAgCSec <161156199+FAgCSec@users.noreply.github.com>
2024-02-25 14:21:27 -0500

    Update script.js

--- codes/script.js
+++ codes/script.js
@@ -1,16 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Obtenemos referencias a elementos HTML relevantes
-    var mainImage = document.getElementById('mainImage');
-    var thumbnails = document.querySelectorAll('.thumbnail');
+// Obtenemos referencias a elementos HTML relevantes
+var mainImage = document.getElementById('mainImage');
+var thumbnails = document.querySelectorAll('.thumbnail');
 
-    // Función para cambiar la imagen principal al hacer clic en una miniatura
-    function changeMainImage(event) {
-        var clickedImageSrc = event.target.src;
-        mainImage.src = clickedImageSrc;
-    }
+// Función para cambiar la imagen principal al hacer clic en una miniatura
+function changeMainImage(event) {
+    mainImage.src = clickedImageSrc;
+}var clickedImageSrc = event.target.src;
+    
 
-    // Agregamos el evento de clic a cada miniatura
-    thumbnails.forEach(function (thumbnail) {
-        thumbnail.addEventListener('click', changeMainImage);
-    });
+// Agregamos el evento de clic a cada miniatura
+thumbnails.forEach(function (thumbnail) {
+    thumbnail.addEventListener('click', changeMainImage);
 });
